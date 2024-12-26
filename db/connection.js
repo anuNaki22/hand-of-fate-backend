8167d9682dee1b936b1b728b27f4d054338adb52
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-26 17:31:06 +0700

    add DB_URL environment variable

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // local
-  user: "postgres",
-  host: "localhost",
-  database: "hof_db2",
-  password: "datalovers",
-  port: 5432,
+  // // local
+  // user: "postgres",
+  // host: "localhost",
+  // database: "hof_db2",
+  // password: "datalovers",
+  // port: 5432,
 
   // deployed
-  //   connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
