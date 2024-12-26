1ba45c9b6cb481e6cc1926370c0f7a9ccaf5753b
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-26 17:57:08 +0700

    ignore ssl

--- db/connection.js
+++ db/connection.js
@@ -10,6 +10,9 @@ const pool = new Pool({
 
   // deployed
   connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false, // Untuk Railway jika menggunakan SSL
+  },
 });
 
 module.exports = pool;
